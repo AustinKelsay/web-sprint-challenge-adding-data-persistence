8790c24733df8ca2985b1edf76fa9770aebca707
Austin Kelsay <53542748+AustinKelsay@users.noreply.github.com>
2020-06-27 11:51:09 -0500

    Finished routers and models (i think) now onto the db, migration, and seeding

---
+++ api/projects-router.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+const Projects = require('./projects-model.js');
+
+
+router.get('/', (req, res) => {
+  Projects.find()
+  .then(projects => {
+    res.json(projects);
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get projects' });
+  });
+});
+
+router.post("/", (req, res) => {
+  const data = req.body;
+  Projects.add(data)
+    .then(project => {
+      res.status(201).json(project);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "server error", err });
+    });
+});
+
+module.exports = router;s
---
+++ api/tasks-model.js
@@ -0,0 +1,23 @@
+const db = require("../data/dbConfig");
+
+const getTasks = () => {
+    return db("tasks as t")
+    .select(
+        "t.id",
+        "t.task_description",
+        "p.project_name",
+        "p.project_description",
+        "t.notes",
+        "t.task_completed"
+        )
+    .join("projects as p", "p.id", "t.projects_id");
+};
+
+const addTask = data => {
+    return db("tasks").insert(data);
+};
+
+module.exports = {
+  getTasks,
+  addTask
+};
